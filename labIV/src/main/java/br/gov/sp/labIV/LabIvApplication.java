c3636ce6026c6474f856e368bb9b07dcbba91792
Marcella Yanes <102261833+yanesmarcella@users.noreply.github.com>
2023-08-15 20:08:15 -0300

    Add files via upload

---
+++ labIV/src/main/java/br/gov/sp/labIV/LabIvApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.labIV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabIvApplication.class, args);
+	}
+
+}
---
+++ labIV/src/test/java/br/gov/sp/labIV/LabIvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.labIV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabIvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
